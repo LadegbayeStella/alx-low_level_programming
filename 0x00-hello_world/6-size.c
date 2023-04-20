767699d75c902834559739b4ed595799ccbb0b6b
Ladegbaye Stella <ladegbayeewatomi50@gmail.com>
2023-04-20 16:58:48 -0700

    push to github task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 
-	printf("Size of a char: %d  byte(s)", seizof(char));
-	printf("Size of an int: %d  byte(s)", seizeof(int));
-	printf("Size of a long int: %d byte(s)", seizeof(long int));
-	printf("Size of a long long int: %d byte(s)", seizeof(long long  int));
-	printf("Size of a float: %d byte(s)", seizeof(float));
+	printf("Size of a char: %lu byte(s)", seizof(char));
+	printf("Size of an int: %lu  byte(s)", seizeof(int));
+	printf("Size of a long int: %lu byte(s)", seizeof(long int));
+	printf("Size of a long long int: %lu  byte(s)", seizeof(long long  int));
+	printf("Size of a float: %lu byte(s)", seizeof(float));
 	return (0);
