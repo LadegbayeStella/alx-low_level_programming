16ce6d0c89a1b87b2c477476d9ae1ab2fe3f4c38
Ladegbaye Stella <ladegbayeewatomi50@gmail.com>
2023-04-20 17:19:08 -0700

    task 7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
-
-	printf("Size of a char: %lu byte(s)", seizof(char));
-	printf("Size of an int: %lu  byte(s)", seizeof(int));
-	printf("Size of a long int: %lu byte(s)", seizeof(long int));
-	printf("Size of a long long int: %lu  byte(s)", seizeof(long long  int));
-	printf("Size of a float: %lu byte(s)", seizeof(float));
+{
+	printf("Size of a char: %lu byte(s)\n", seizof(char));
+	printf("Size of an int: %lu  byte(s)\n", seizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", seizeof(long int));
+	printf("Size of a long long int: %lu  byte(s)\n", seizeof(long long  int));
+	printf("Size of a float: %lu byte(s)\n", seizeof(float));
 	return (0);
+}
