fd395c1c158b552ee59a3468cef6a2ed34b7559e
Ladegbaye Stella <ladegbayeewatomi50@gmail.com>
2023-04-24 21:59:40 -0700

    pushing

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - prints the alphabet in lowercase, and then in uppercase,
  * followed by a new line
@@ -10,15 +9,10 @@ int main(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-
+	{
 		putchar(ch);
-
-	for (ch = 'A'; ch <= 'Z'; ch++)
-
-		putchar(ch);
-
-	putchar('\n');
-
+	}
+	putchar ('\n');
 	return (0);
-
 }
+	
