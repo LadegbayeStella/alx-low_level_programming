d3a35654092ea55f6e5276c8c780e07a2a2a6aef
Ladegbaye Stella <ladegbayeewatomi50@gmail.com>
2023-04-28 13:41:03 -0700

    pushing task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,12 +3,11 @@
  * print_triangle - prints a triangle, followed by a new line
  * @size: size of the triangle
  */
-
 void print_triangle(int size)
 {
 	if (size <= 0)
 	{
-		 _putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
@@ -16,15 +15,16 @@ void print_triangle(int size)
 
 		for (i = 1; i <= size; i++)
 		{
-			for (j = i; j < size; i++)
+			for (j = i; j < size; j++)
 			{
-				 _putchar(' ');
+				_putchar(' ');
 			}
 
 			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
 			}
+
 			_putchar('\n');
 		}
 	}
