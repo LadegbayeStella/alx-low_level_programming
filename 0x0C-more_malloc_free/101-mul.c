b14a0086b3e42c8195164096985f88a072ce11e6
Ladegbaye Stella <ladegbayeewatomi50@gmail.com>
2023-05-18 17:09:14 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -34,3 +34,21 @@ void _is_zero(char *argv[])
 	}
 }
 
+/**
+ * _initialize_array - set memery to zero in a new array
+ * @ar: char array.
+ * @lar: length of the char array.
+ *
+ * Return: pointer of a char array.
+ */
+char *_initialize_array(char *ar, int lar)
+{
+	int i = 0;
+
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
+	ar[lar] = '\0';
+	return (ar);
+}
+
+
